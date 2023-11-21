818aeb545a7bedca980b1950c0e408825d36d23e
JimmyDeveloperMan <jimmy.moftah@gmail.com>
2023-11-21 16:52:58 +0200

    lol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // function redirectToGallery() {
-  //   return redirect("/gallery");
-  // }
-  // redirectToGallery();
+  function redirectToGallery() {
+    return redirect("/gallery");
+  }
+  redirectToGallery();
   return <main></main>;
 }
